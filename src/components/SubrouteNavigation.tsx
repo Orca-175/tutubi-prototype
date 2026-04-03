8dd614cc06259ee38e57f9fb60540b8c42972e6d
Orca-175 <stephorcaa@gmail.com>
2026-04-03 18:40:53 +0800

    Make subroute navigation component prettier

--- src/components/SubrouteNavigation.tsx
+++ src/components/SubrouteNavigation.tsx
@@ -1,13 +1,26 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 interface Props {
   items: { name: string, route: string }[];
 }
 
 export function SubrouteNavigation({ items }: Props) {
-  const html = items.map((item) => (
-    <Link to={item.route}><span className="text-button">{item.name}</span></Link>
-  ));
+  const location = useLocation();
+
+  const html = items.map((item, index) => {
+    return (
+      <>
+        <Link to={item.route}>
+          <span 
+            className={`text-button ${location.pathname != item.route && "text-button-inactive"}`}
+          >
+            {item.name}
+          </span>
+        </Link>
+        {(index != items.length - 1) && <span>|</span>}
+      </>
+    );
+  });
 
   return (
     <div className="subroute-nav">{html}</div>
