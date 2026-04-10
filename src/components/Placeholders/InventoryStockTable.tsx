b3329e2b1863bc78b17495dd50284805a4cd05b5
Orca-175 <stephorcaa@gmail.com>
2026-04-10 20:41:20 +0800

    Finish prototype of stock table

--- src/components/Placeholders/InventoryStockTable.tsx
+++ src/components/Placeholders/InventoryStockTable.tsx
@@ -1,63 +1,88 @@
 import styles from "./InventoryStockTable.module.scss";
 import { pesoSign } from "../../constants/symbols";
 import { parentTableKeys, childTableKeys, data, groupRows } from "../../utils/prototyping/mockDatabase";
-import type { ReactElement } from "react";
+import { useState, type ReactElement } from "react";
 
 export function InventoryStockTable() {
   const tableData = groupRows(data);
-  const finalTableRows: ReactElement[] = [];
+  const tableRows: ReactElement[] = [];
+
+  // On click on row, check if product name is in expanded array. If so, expand.
+  const [expanded, setExpanded] = useState<string[]>([]);
 
   for (const [productName, productInfo] of Object.entries(tableData)) {
+    tableRows.push(
+      <>
+        {/* Parent table row */}
+        <tr 
+          className={styles.parentTableRow} 
+          key={productName + "parent"}
+          onClick={() => {
+            // Adds/removes productName from expanded
+            if (expanded.includes(productName)) {
+              setExpanded(expanded.toSpliced(expanded.indexOf(productName), 1));
+            } else {
+              setExpanded([...expanded, productName]);
+            }
+          }} 
+        >
+          <td>{productName}</td>
+          <td>{productInfo.totalCurrentQuantity}</td>
+          <td>{productInfo.totalOutgoingQuantity}</td>
+          <td>{pesoSign}{productInfo.price}</td>
+        </tr>
 
-    finalTableRows.push(
-      <tr>
-        <td>{productName}</td>
-        <td>{productInfo.totalCurrentQuantity}</td>
-        <td>{productInfo.totalOutgoingQuantity}</td>
-        <td>{pesoSign}{productInfo.price}</td>
-      </tr>
+        {/* Child table row */}
+        {
+          expanded.includes(productName) && 
+            <tr className={styles.childTableContainer} key={productName + "children"}> {/* Row of parent table that contains child table */}
+              <td colSpan={4}>
+                <div>
+                  <table> { /* Actual child table */}
+                    <thead>
+                      <tr>
+                        {childTableKeys.map((item) => (
+                          <th key={item}>{item}</th>
+                        ))}
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {productInfo.inventoryRows.map((row) => (
+                        <tr key={row.batchNumber}>
+                          {Object.entries(row).map((entry) => {
+                            const [ columnName, value ] = entry;
+                            if (columnName == "currentQuantity" || columnName == "outgoingQuantity") {
+                              return <td key={row.batchNumber + columnName}>{value}kg</td>;
+                            } else if (columnName == "price") {
+                              return <td key={row.batchNumber + columnName}>{pesoSign}{value}</td>;
+                            } else {
+                              return <td key={row.batchNumber + columnName}>{value}</td>
+                            }
+                          })}
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </td>
+            </tr>
+        }
+      </>
     );
   }
 
   return (
     <table className={styles.stockTable}>
-      <tbody>
+      <thead>
         <tr>
-          {parentTableKeys.map((item) => (
-            <th key={item}>{item}</th>
+          {parentTableKeys.map((header) => (
+            <th key={header}>{header}</th>
           ))}
         </tr>
-        {finalTableRows}
+      </thead>
+      <tbody>
+        {tableRows}
       </tbody>
     </table>
   );
 }
-
-// const tableData = data;
-
-// return (
-//   <table className={styles.stockTable}>
-//     <tbody>
-//       <tr>
-//         {childTableKeys.map((item) => (
-//           <th key={item}>{item}</th>
-//         ))}
-//       </tr>
-//       {tableData.map((tableData) => (
-//         <tr key={tableData.batchNumber}>
-//           {Object.entries(tableData).map((entry) => {
-//             const [ columnName, value ] = entry;
-
-//             if (columnName == "currentQuantity" || columnName == "outgoingQuantity") {
-//               return <td key={tableData.batchNumber + columnName}>{value}kg</td>;
-//             } else if (columnName == "price") {
-//               return <td key={tableData.batchNumber + columnName}>{pesoSign}{value}</td>;
-//             } else {
-//               return <td key={tableData.batchNumber + columnName}>{value}</td>
-//             }
-//           })}
-//         </tr>
-//       ))}
-//     </tbody>
-//   </table>
-// );
