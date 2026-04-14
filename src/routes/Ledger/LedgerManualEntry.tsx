34c9a3f1ab456b0a47e691a8f889af60854d12bc
Orca-175 <stephorcaa@gmail.com>
2026-04-14 16:26:18 +0800

    Add manual entry subroute in ledger page

--- src/routes/Ledger/LedgerManualEntry.tsx
+++ src/routes/Ledger/LedgerManualEntry.tsx
@@ -1,5 +1,33 @@
+import { InputDropdown } from "../../components/FormsControls/inputDropdown";
+import { InputSubmit } from "../../components/FormsControls/InputSubmit";
+import { InputText } from "../../components/FormsControls/InputText";
+import { TransactionsTable } from "../../components/Placeholders/TransactionsTable";
+
+
 export function LedgerManualEntry() {
+  const transactionTypeChoices = [
+  { label: "Sale", value: "value" },
+  { label: "Purchase", value: "value" },
+  ];
+
   return (
-    <div>Manual Entry</div>
+    <>
+      <form className="common-page-form">
+        <div className="common-page-inputs-container">
+          <InputText label="Transaction Date" />
+          <InputDropdown label="Transaction Type" choices={transactionTypeChoices} />
+          <InputText label="Party" />
+          <InputText label="Price" />
+          <InputText label="Product" />
+          <InputText label="Quantity" />
+        </div>
+        <InputSubmit label="Record Transaction" />
+      </form>
+      <div>
+      </div>
+      <div className="table-card">
+        <TransactionsTable />
+      </div>
+    </>
   );
 }
