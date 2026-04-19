762ccda6d0129d263a4328a701484f0a4f27abed
Orca-175 <stephorcaa@gmail.com>
2026-04-20 00:59:50 +0800

    Make .page-layout class utilize screen real estate better on some page

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -10,35 +10,37 @@ export function Home() {
   return (
     <>
       <div className="page-header">{PAGE_NAMES.home}</div>
-      <div className={styles.dashboard}>
-        <div className={styles.highlightCards}>
-          <DashboardCard title="Upcoming Orders" route={ROUTES.orders}>
-            <p className={styles.cardContent}>27</p>
-          </DashboardCard>
-          <DashboardCard title="Near Expiry" route={ROUTES.inventory}>
-            <p className={styles.cardContent}>12</p>
-          </DashboardCard>
-          <DashboardCard title="Total Worth" route={ROUTES.inventory}>
-            <p className={styles.cardContent}>{pesoSign}312,025</p>
-          </DashboardCard>
-        </div>
-        <div>
-          <DashboardCard title="Active Orders" route={ROUTES.orders}>
-            <PHOrdersTable/>
-          </DashboardCard>
-        </div>
-        <div>
-          <DashboardCard title="Messages" route={ROUTES.clients}>
-            <div className={styles.messagesRowsContainer}>
-              <PHMessages />
-              <PHMessages />
-              <PHMessages />
-              <PHMessages />
-              <PHMessages />
-              <PHMessages />
-              <PHMessages />
-            </div>
-          </DashboardCard>
+      <div className="page-layout">
+        <div className={styles.dashboard}>
+          <div className={styles.highlightCards}>
+            <DashboardCard title="Upcoming Orders" route={ROUTES.orders}>
+              <p className={styles.cardContent}>27</p>
+            </DashboardCard>
+            <DashboardCard title="Near Expiry" route={ROUTES.inventory}>
+              <p className={styles.cardContent}>12</p>
+            </DashboardCard>
+            <DashboardCard title="Total Worth" route={ROUTES.inventory}>
+              <p className={styles.cardContent}>{pesoSign}312,025</p>
+            </DashboardCard>
+          </div>
+          <div>
+            <DashboardCard title="Active Orders" route={ROUTES.orders}>
+              <PHOrdersTable/>
+            </DashboardCard>
+          </div>
+          <div>
+            <DashboardCard title="Messages" route={ROUTES.clients}>
+              <div className={styles.messagesRowsContainer}>
+                <PHMessages />
+                <PHMessages />
+                <PHMessages />
+                <PHMessages />
+                <PHMessages />
+                <PHMessages />
+                <PHMessages />
+              </div>
+            </DashboardCard>
+          </div>
         </div>
       </div>
     </>
